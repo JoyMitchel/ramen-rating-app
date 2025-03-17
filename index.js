df08f8623c955947d34fcf9df8a35db9418f72e2
Joy <joy.mitchel@student.moringaschool.com>
2025-03-17 15:28:16 +0300

    new commit

---
+++ index.js
@@ -0,0 +1,67 @@
+const ramens = [
+    { id: 1, name: "Gyukotsu", restaurant: "Ichiran", image: "images/gyukotsu.jpg", rating: 5, comment: "Delicious!" },
+    { id: 2, name: "Kojiro", restaurant: "Menya", image: "images/kojiro.jpg", rating: 4, comment: "Very flavorful!" },
+    { id: 3, name: "Naruto", restaurant: "Ramen-ya", image: "images/naruto.jpg", rating: 3, comment: "Good, but a bit salty." },
+    { id: 4, name: "Nirvana", restaurant:"Meko", image:"images/nirvana.jpg", rating: 5, comment: "Amazing!" },
+    { id: 5, name: "Shoyu", restaurant:"Branch", image: "images/shoyu.jpg", rating: 3, comment:"Good!" }
+  ];
+  
+  function displayRamens() {
+    const ramenMenu = document.getElementById("ramen-menu");
+  
+    ramens.forEach(ramen => {
+      const ramenImg = document.createElement("img");
+      ramenImg.src = ramen.image;
+      ramenImg.alt = ramen.name;
+    
+      ramenMenu.appendChild(ramenImg);
+    });
+  }
+  
+  function handleClick(event) {
+    const ramenId = event.target.dataset.id;
+    const selectedRamen = ramens.find(ramen => ramen.id == ramenId);
+  
+    if (selectedRamen) {
+      document.getElementById("ramen-name").textContent = `Name: ${selectedRamen.name}`;
+      document.getElementById("ramen-restaurant").textContent = `Restaurant: ${selectedRamen.restaurant}`;
+      document.getElementById("ramen-rating").textContent = `Rating: ${selectedRamen.rating}`;
+      document.getElementById("ramen-comment").textContent = `Comment: ${selectedRamen.comment}`;
+    }
+  }
+  
+  function addSubmitListener() {
+    const form = document.getElementById("ramen-form");
+  
+    form.addEventListener("submit", function(event) {
+      event.preventDefault();
+  
+      const newRamen = {
+        id: ramens.length + 1,
+        name: document.getElementById("name").value,
+        restaurant: document.getElementById("restaurant").value,
+        rating: parseInt(document.getElementById("rating").value),
+        comment: document.getElementById("comment").value
+      };
+  
+    
+  
+      const ramenMenu = document.getElementById("ramen-menu");
+      const ramenImg = document.createElement("img");
+      ramenImg.src = newRamen.image;
+      ramenImg.alt = newRamen.name;
+      ramenImg.classList.add("ramen-img");
+      ramenImg.dataset.id = newRamen.id;
+      ramenMenu.appendChild(ramenImg);
+    });
+  }
+  
+  function main() {
+    displayRamens();
+    addSubmitListener();
+    document.getElementById("ramen-menu").addEventListener("click", handleClick);
+  }
+  
+  document.addEventListener("DOMContentLoaded", main);
+  
+
